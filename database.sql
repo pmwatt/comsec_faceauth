6629a733a597fc8d950403f65a45e7d001b58b67
pmwatt <prachnachai.mek@student.mahidol.edu>
2024-04-06 17:54:15 +0700

    add sql to create db/table

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS comsec_faceauth;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    face_data BLOB NOT NULL
+);
+
+-- for testing
+SELECT * FROM users;
